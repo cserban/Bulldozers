168512c3f7d0396da6eb3098a1feb059731658b1
unknown <serban.chiricescu@gmail.com>
2013-04-17 07:37:04 -0700

    new groups class

---
+++ src/Groups.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+
+public class Groups {
+	public ArrayList<LoadDocument> documents;
+	Groups () throws FileNotFoundException
+	{
+		 CSVReader reader = null;
+		 documents = new ArrayList<>();
+		 
+		 reader = new CSVReader(new FileReader("train/Train_BL.csv"));
+		 documents.add(new LoadDocument(reader));
+		 
+		 reader = new CSVReader(new FileReader("train/Train_MG.csv"));
+		 documents.add(new LoadDocument(reader));
+		 
+		 reader = new CSVReader(new FileReader("train/Train_SSL.csv"));
+		 documents.add(new LoadDocument(reader));
+		 
+		 reader = new CSVReader(new FileReader("train/Train_TEX.csv"));
+		 documents.add(new LoadDocument(reader));
+		 
+		 reader = new CSVReader(new FileReader("train/Train_TTT.csv"));
+		 documents.add(new LoadDocument(reader));
+		 
+		 reader = new CSVReader(new FileReader("train/Train_WL.csv"));
+		 documents.add(new LoadDocument(reader));
+	}
+}
